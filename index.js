0a758b1f2238e30fa667bafb87592149e0ab4221
Sotirios Anastasiadis <64400907+sotirisanastjm@users.noreply.github.com>
2022-04-22 12:18:45 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+    if(!$("#myCanvas").tagcanvas({
+        textColour: "#08fdd8",
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        weight: true,
+    }, "tags")){
+        // something went wrong hide the canvas container,
+        $("#myCanvasContainer");
+    }
+})
